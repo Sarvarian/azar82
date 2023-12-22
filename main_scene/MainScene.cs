bd635ac386eeb48fc0b149b582273f6acd36ee24
Sarvarian <Sarvarian@Hotmail.com>
2023-12-22 05:19:08 -0100

    Update MainScene.cs to check for exit request

--- main_scene/MainScene.cs
+++ main_scene/MainScene.cs
@@ -11,24 +11,35 @@ public sealed partial class MainScene : Node
         base._Ready();
         main_ = new agents.Main(this, GetViewport());
         main_.Start();
+        CheckExitRequest();
     }
 
     public override void _Process(double delta)
     {
         base._Process(delta);
         main_!.Process(delta);
+        CheckExitRequest();
     }
 
     public override void _PhysicsProcess(double delta)
     {
         base._PhysicsProcess(delta);
         main_!.PhysicsProcess(delta);
+        CheckExitRequest();
     }
 
     protected override void Dispose(bool disposing)
     {
         base.Dispose(disposing);
         main_!.End();
+    }
+    
+    private void CheckExitRequest()
+    {
+        if (agents.ExitRequest.DoExit)
+        {
+            GetTree().Quit();
+        }
     }
 
 }
