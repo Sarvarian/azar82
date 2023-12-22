7b2ac9902b0bb31dbbbba8e0dc5d512b740f9551
Sarvarian <Sarvarian@Hotmail.com>
2023-12-22 05:04:51 -0100

    Update MainScene.cs

--- main_scene/MainScene.cs
+++ main_scene/MainScene.cs
@@ -1,4 +1,3 @@
-using azar82.scripts_godot_agents;
 using Godot;
 
 namespace azar82.main_scene;
@@ -10,26 +9,26 @@ public sealed partial class MainScene : Node
     public override void _Ready()
     {
         base._Ready();
-        main_ = new Main(this, GetViewport());
+        main_ = new scripts_godot_agents.Main(this, GetViewport());
         main_.Start();
     }
 
     public override void _Process(double delta)
     {
         base._Process(delta);
-        main_?.Process(delta);
+        main_!.Process(delta);
     }
 
     public override void _PhysicsProcess(double delta)
     {
         base._PhysicsProcess(delta);
-        main_?.PhysicsProcess(delta);
+        main_!.PhysicsProcess(delta);
     }
 
     protected override void Dispose(bool disposing)
     {
         base.Dispose(disposing);
-        main_?.End();
+        main_!.End();
     }
 
 }
